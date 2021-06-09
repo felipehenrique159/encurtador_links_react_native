150f7493ddd6a2971f3be19052d4a52a84c64168
felipehenrique159 <felipecod6@hotmail.com>
2021-06-09 17:50:12 -0300

    Componente Lista de links

---
+++ src/components/ListItem/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View,Text} from 'react-native'
+import {} from './styles'
+
+
+
+export default function ListItem(){
+    return(
+        <View>
+            <Text>TESTE</Text>
+        </View>       
+    )
+}
--- src/pages/MyLinks/index.js
+++ src/pages/MyLinks/index.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import {View,Text} from 'react-native'
 import StatusBarPage from '../../components/StatusBarPage'
+import {Container, Title,ListLinks} from './styles'
+import Menu from '../../components/Menu'
+import ListItem from '../../components/ListItem'
+
 
 export default function MyLinks(){
     return(
-        <View>
+        
+        <Container>
 
-        <StatusBarPage
-            barStyle="light-content"
-            backgroundColor="#132742"
-        />
+            <StatusBarPage
+                barStyle="light-content"
+                backgroundColor="#132742"
+            />
 
-            <Text>Pagina MyLinks</Text>
-        </View>
+            <Menu/>
+
+            <Title>Meus links</Title>
+            <ListLinks
+                data={[{id:1 , link:'teste.com'},{id:2 , link:'teste2.com'},{id:3, link:'teste2.com'}]}
+                keyExtractor={ (item) => String(item.id) }
+                renderItem={ ({item}) => <ListItem/> }
+                contentContainerStyle={{paddingBottom:20}}
+                showsVerticalScrollIndicator={false}
+            />
+
+        </Container>
     )
 }
